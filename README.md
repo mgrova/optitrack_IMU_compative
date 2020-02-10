cd5b1428b323e989862113aea78b322e793b617a
marco <marrcogrova@gmail.com>
2020-02-10 13:29:04 +0100

    added serial port read

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # optitrack_IMU_compative
+
+Example usage:
+```
+mkdir build ; cd build
+cmake .. ; make -j{nproc}
+
+./devel/lib/optitrack_IMU_comparative/optitrack_IMU_comparative "/dev/ttyACM0" "115200"
+```
--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,8 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/TwistStamped.h>
+
+#include <serial/serial.h>
 
 #include <Eigen/Eigen>
 
@@ -31,24 +32,58 @@
 
 int main(int _argc , char **_argv){
 
-    ros::init(_argc, _argv, "T265_VELOCITY");
+    if (_argc < 3){
+        std::cout << "Expected port and baudrate passed by command line. \n";
+        return 0;
+    }
+
+    ros::init(_argc, _argv, "OPTITRACK_IMU_COMPARATION");
 
     ros::NodeHandle nh;
-    ros::Publisher pubVel = nh.advertise<geometry_msgs::TwistStamped>("/velocity_estimated", 1);
+
+    Eigen::Vector3f pose;
+    Eigen::Quaternionf orientation;
 
     ros::Subscriber subPose = nh.subscribe<geometry_msgs::PoseStamped>("/optitrack/pose",   1, [&](const geometry_msgs::PoseStamped::ConstPtr& _msg){
+        pose[0] = _msg->pose.position.x;
+        pose[1] = _msg->pose.position.y;
+        pose[2] = _msg->pose.position.z;
 
+        orientation.w() =  _msg->pose.orientation.w;
+        orientation.x() =  _msg->pose.orientation.x;
+        orientation.y() =  _msg->pose.orientation.y;
+        orientation.z() =  _msg->pose.orientation.z;
 
     });
+    
+    // Read IMU using serial port
+    serial::Serial ser;
+    std::string port = _argv[1];
+    int baudrate = atoi(_argv[2]);
+    try
+    {
+        ser.setPort(port);
+        ser.setBaudrate(baudrate);
+        serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+        ser.setTimeout(to);
+        ser.open();
+    }
+    catch (serial::IOException& e)
+    {
+        std::cout << "Unable to open port " << "\n";
+        return -1;
+    }
 
-    ros::Subscriber subOrientation = nh.subscribe<geometry_msgs::PoseStamped>("/optitrack/pose",   1, [&](const geometry_msgs::PoseStamped::ConstPtr& _msg){
-
-
-    });
+    // Check if is sucessfully open
+    if(ser.isOpen()){
+        std::cout << "Serial Port initialized" << "\n";
+    }else{
+        return -1;
+    }
 
     ros::AsyncSpinner spinner(4);
     spinner.start();
-    ros::Rate r(100); // set 100 Hz 
+    ros::Rate r(50); // set 100 Hz 
     
     // Open log file
     std::ofstream log;
@@ -57,11 +92,16 @@ int main(int _argc , char **_argv){
     auto t0 = std::chrono::system_clock::now();
     while(ros::ok()){
 
+        ros::spinOnce();
+
+        if(ser.available()){
+            std::string result = ser.readline();
+            if (result != "")
+                std::cout << "Read from serial port: " << result << "\n";
+        }
+
         auto t1 = std::chrono::system_clock::now();
         float incT = float(std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count())/1000.0;
-        
-        ros::spinOnce();
-        
         // Save dataset
         log << std::to_string( 0.0 ) + " " + std::to_string( float(0.0) ) + " " + std::to_string( float(0.0) ) + " " + "\n";
         log.flush();
